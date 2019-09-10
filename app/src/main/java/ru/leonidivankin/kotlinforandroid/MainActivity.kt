193c18b7418c94fdb369b3bffe772b40e697439a
LeonidIvankin <leonid.ivankin@fsight.ru>
2019-09-10 10:17:35 +0300

    migrated to Kotlin

---
+++ app/src/main/java/ru/leonidivankin/kotlinforandroid/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.leonidivankin.kotlinforandroid
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
